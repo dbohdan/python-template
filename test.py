b08b23a46b01cf831f61282264f362b1bae14325
D. Bohdan <dbohdan@dbohdan.com>
2023-09-26 11:01:07 +0000

    test: test with tox and nox
    Replace `project_name` with a more accurate `project_slug`.

--- test.py
+++ test.py
@@ -1,38 +1,52 @@
 #! /usr/bin/env python3
 
+from __future__ import annotations
+
 import os
 import shutil
 import subprocess as sp
 from pathlib import Path
 
-
+DATA = {
+    "project_slug": "foo",
+    "package_name": "baz",
+}
 TEST_DIR = "test"
+
+
+def run(*args: str, **kwargs) -> sp.CompletedProcess:
+    return sp.run(args, check=True, **kwargs)
+
+
+def data() -> list[str]:
+    return [x for key, value in DATA.items() for x in ("--data", f"{key}={value}")]
 
 
 def main() -> None:
     root = Path(__file__).resolve().parent
-    test_dir = root / "test"
+    test_dir = root / TEST_DIR
 
     os.chdir(root)
     if test_dir.is_dir():
         shutil.rmtree(test_dir)
-    sp.run(
-        [
-            "copier",
-            "copy",
-            "-d",
-            "project_name=foo",
-            "-d",
-            "package_name=baz",
-            "template",
-            TEST_DIR,
-        ],
-        check=True,
+
+    run(
+        "copier",
+        "copy",
+        *data(),
+        "template",
+        TEST_DIR,
     )
 
     os.chdir(root / TEST_DIR)
-    sp.run(["poetry", "install"], check=True)
-    sp.run(["poetry", "run", "pytest"], check=True)
+    try:
+        run("poetry", "install")
+        run("poetry", "run", "baz")
+    finally:
+        run("poetry", "env", "remove", "--all")
+
+    run("tox", "run", "-e", "py38")
+    run("nox", "--pythons", "3.8")
 
 
 if __name__ == "__main__":
