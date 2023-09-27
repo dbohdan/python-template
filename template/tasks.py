17ebae799ab094b8096d06dad21d409aca4ba422
D. Bohdan <dbohdan@dbohdan.com>
2023-09-27 15:27:28 +0000

    refactor(tasks): rewrite with Click
    I should not Greenspun Click just to avoid a dependency.
    The user can install with pip it or run `tasks.py` through Poetry.

--- template/tasks.py
+++ template/tasks.py
@@ -1,34 +1,23 @@
 #! /usr/bin/env python3
-# Project tasks.
-# This file must only depend on the Python standard library.
 
-from __future__ import annotations
-
-import argparse
 import os
 import re
 import shlex
 import subprocess as sp
+import sys
 from pathlib import Path
-from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
-    from collections.abc import Callable, Generator
-
-    Task = Callable[..., None]
+import click
 
 
-tasks: dict[str, Task] = {}
-
-
-def run(*args: str | Path, check: bool = True, quiet: bool = False) -> None:
+def run(*args, check=True, quiet=False):
     if not quiet:
         print(">", shlex.join(str(arg) for arg in args))
 
     sp.run(args, check=check)
 
 
-def source_files() -> Generator[Path, None, None]:
+def source_files():
     return (
         path
         for path in Path().glob("**/*.py")
@@ -36,35 +25,33 @@ def source_files() -> Generator[Path, None, None]:
     )
 
 
-def task(*, name: str = ""):
-    def inner(f: Task) -> Task:
-        nonlocal name
-        tasks[name if name else f.__name__.replace("_", "-")] = f
-
-        return f
-
-    return inner
+@click.group()
+def cli():
+    pass
 
 
-@task()
-def check(*, fix: bool = False) -> None:
-    format_()
-    lint(fix=fix)
-    type_()
+@cli.command()
+@click.option("--fix", is_flag=True, help="Fix linter errors")
+@click.pass_context
+def check(ctx, fix: bool = False) -> None:
+    ctx.invoke(format_)
+    ctx.invoke(lint, fix=fix)
+    ctx.invoke(type_)
 
 
-@task()
+@cli.command()
 def files() -> None:
     print("\n".join(str(path) for path in source_files()))
 
 
-@task(name="format")
+@cli.command(name="format")
 def format_() -> None:
-    run("poetry", "run", "black", *source_files(), check=False)
+    run("poetry", "run", "black", *source_files())
 
 
-@task()
-def lint(*, fix: bool = False) -> None:
+@cli.command()
+@click.option("--fix", is_flag=True, help="fix linter errors")
+def lint(fix: bool = False) -> None:
     extra = ["--fix"] if fix else []
     run(
         "poetry",
@@ -72,11 +59,10 @@ def lint(*, fix: bool = False) -> None:
         "ruff",
         *extra,
         *source_files(),
-        check=False,
     )
 
 
-@task()
+@cli.command()
 def test() -> None:
     run(
         "poetry",
@@ -86,45 +72,22 @@ def test() -> None:
     )
 
 
-@task(name="type")
+@cli.command(name="type")
 def type_() -> None:
     run(
         "poetry",
         "run",
         "pyright",
         *source_files(),
-        check=False,
     )
 
 
-def cli() -> argparse.Namespace:
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(required=True)
-
-    spar_by_task = {}
-    for name, func in tasks.items():
-        subparser = subparsers.add_parser(name)
-        subparser.set_defaults(func=func)
-        spar_by_task[name] = subparser
-
-    for task in ("check", "lint"):
-        spar_by_task[task].add_argument(
-            "--fix",
-            action="store_true",
-            default=False,
-            help="fix linter errors",
-        )
-
-    return parser.parse_args()
-
-
-def main():
+def main() -> None:
     os.chdir(Path(__file__).resolve().parent)
-
-    args = cli()
-    args_dict = vars(args)
-    func_args = {key: args_dict[key] for key in args_dict if key != "func"}
-    args.func(**func_args)
+    try:
+        cli()
+    except sp.CalledProcessError as e:
+        sys.exit(e.returncode)
 
 
 if __name__ == "__main__":
