2f6580dce167756a04dab40219376e35583be4aa
D. Bohdan <dbohdan@dbohdan.com>
2023-09-27 21:42:19 +0000

    refactor(tasks/files): move to script

---
+++ template/scripts/tasks.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+import os
+import shlex
+from pathlib import Path
+
+
+def source_files() -> list[Path]:
+    srcs = os.environ["PYTHON_SOURCES"]
+
+    files = []
+    for x in shlex.split(srcs):
+        path = Path(x)
+        if path.is_dir:
+            files.extend(path.glob("*.py"))
+        else:
+            files.append(path)
+
+    return sorted(files)
+
+
+def files() -> None:
+    print("\n".join(str(path) for path in source_files()))
