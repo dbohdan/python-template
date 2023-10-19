ca551e3d7a859f6d3bba676379eae6107ab3c69a
D. Bohdan <dbohdan@dbohdan.com>
2023-10-19 08:51:06 +0000

    build(nox): use nox-poetry
    Use an exact nox and nox-poetry dev dependency version
    to ensure they are compatible with each other.

--- template/noxfile.py
+++ template/noxfile.py
@@ -1,11 +1,14 @@
+# Requires nox-poetry.
+# https://github.com/cjolowicz/nox-poetry
+
 from __future__ import annotations
 
-import nox
+from nox_poetry import Session, session
 
 PYTHON_VERSIONS = ["3.8", "3.9", "3.10", "3.11"]
 
 
-@nox.session(python=PYTHON_VERSIONS, tags=["test"])
-def tests(session: nox.Session) -> None:
-    session.run("poetry", "install", "--no-root", "--sync", external=True)
+@session(python=PYTHON_VERSIONS)
+def tests(session: Session) -> None:
+    session.install(".", "pytest")
     session.run("pytest")
