ee0a5e0f26b43a2bbe7d52b3cb038d82bb8bf9f0
D. Bohdan <dbohdan@dbohdan.com>
2023-11-02 13:09:56 +0000

    build(nox): test through `poe test`

--- template/noxfile.py
+++ template/noxfile.py
@@ -10,5 +10,6 @@ PYTHON_VERSIONS = ["3.8", "3.9", "3.10", "3.11", "3.12"]
 
 @session(python=PYTHON_VERSIONS)
 def tests(session: Session) -> None:
-    session.install(".", "pytest")
-    session.run("pytest")
+    session.run_always("poetry", "install", external=True)
+
+    session.run("poe", "test", external=True)
