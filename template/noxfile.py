fd4a73348882cf115034e0773a4c819c45ed7741
D. Bohdan <dbohdan@dbohdan.com>
2024-01-14 12:14:50 +0000

    build(nox): do not run Poetry inside test env

--- template/noxfile.py
+++ template/noxfile.py
@@ -1,15 +1,11 @@
-# Requires nox-poetry.
-# https://github.com/cjolowicz/nox-poetry
-
 from __future__ import annotations
 
-from nox_poetry import Session, session
+from nox import Session, session
 
 PYTHON_VERSIONS = ["3.8", "3.9", "3.10", "3.11", "3.12"]
 
 
 @session(python=PYTHON_VERSIONS)
 def tests(session: Session) -> None:
-    session.run_always("poetry", "install", external=True)
-
-    session.run("poe", "test", external=True)
+    session.install(".", "pytest>=7,<8")
+    session.run("pytest")
